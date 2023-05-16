8d3b12ba7a3b917fadcd061f0778236c464f1667
Keith Lê <87120816+LeDongAnhKiet@users.noreply.github.com>
2023-05-16 17:31:36 +0700

    Sắp xếp code ngăn nắp

---
+++ Sắp xếp/Shader.h
@@ -0,0 +1,27 @@
+﻿#ifndef SHADER_CLASS_H
+#define SHADER_CLASS_H
+
+#include <glad/glad.h>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <cerrno>
+
+std::string get_content(const char* file);
+
+class Shader
+{
+public:
+	// tham chiếu vào Shader
+	GLuint ID;
+	// khởi tạo Shader = 2 file
+	Shader(const char* vertexFile, const char* fragmentFile);
+
+	// kích hoạt Shader
+	void Activate();
+	// xóa Shader
+	void Delete();
+};
+
+#endif

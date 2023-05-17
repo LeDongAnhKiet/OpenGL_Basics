00cdfcf76d35056abd4c5c72d72b30c0fb77085a
Keith Lê <87120816+LeDongAnhKiet@users.noreply.github.com>
2023-05-17 17:51:47 +0700

    Chạy hình ảnh lên window

--- 06 - Hình ảnh/06 - HinhAnh.cpp
+++ 06 - Hình ảnh/06 - HinhAnh.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include "ElementsBufferObject.h"
@@ -11,14 +11,14 @@
 // tọa độ các điểm
 GLfloat vertices[] =
 {
-					// điểm trái dưới
-	-0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f,
-					// điểm trái trên
-	-0.5f, 0.5f , 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,
-					// điểm phải trên
-	0.5f , 0.5f , 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,
-					// điểm phải dưới
-	0.5f , -0.5f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f
+		// điểm trái dưới
+	-0.5f, -0.5f, 0.0f,	1.0f, 0.0f, 0.0f,	0.0f, 0.0f,
+		// điểm trái trên
+	-0.5f, 0.5f , 0.0f,	0.0f, 1.0f, 0.0f,	0.0f, 1.0f,
+		// điểm phải trên
+	0.5f , 0.5f , 0.0f,	0.0f, 0.0f, 1.0f,	1.0f, 1.0f,
+		// điểm phải dưới
+	0.5f , -0.5f, 0.0f,	1.0f, 1.0f, 1.0f,	1.0f, 0.0f
 };
 // thứ tự các điểm
 GLuint indices[] =
@@ -33,9 +33,14 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-	GLFWwindow* window = glfwCreateWindow(800, 800, "Hinh anh", NULL, NULL);
-	if (window == NULL) glfwTerminate();
+	
+	GLFWwindow* window = glfwCreateWindow(800, 800, "YoutubeOpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window\n";
+		glfwTerminate();
+		return -1;
+	}
 	glfwMakeContextCurrent(window);
 	gladLoadGL();
 	glViewport(0, 0, 800, 800);
@@ -46,30 +51,29 @@ int main()
 	VBO vbo(vertices, sizeof(vertices));
 	EBO ebo(indices, sizeof(indices));
 
-	vao.LinkAttr(vbo, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)0);
-	vao.LinkAttr(vbo, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-	vao.LinkAttr(vbo, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-	
+	vao.LinkAttrib(vbo, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)0);
+	vao.LinkAttrib(vbo, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	vao.LinkAttrib(vbo, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+
 	vao.Unbind();
 	vbo.Unbind();
 	ebo.Unbind();
 
-	GLuint uni = glGetUniformLocation(shader.ID, "scale");
-	// hình ảnh
-	Texture kirby("kirby.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
-	kirby.txUnit(shader, "tex", 0);
+	GLuint uniID = glGetUniformLocation(shader.ID, "scale");
+	// khởi tạo hình ảnh
+	Texture popCat("pop_cat.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
+	popCat.texUnit(shader, "tex0", 0);
 
 	while (!glfwWindowShouldClose(window))
 	{
-		glClearColor(0.25f, 0.12f, 0.58f, 0.5f);
+		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-		shader.Activate(); 
+		shader.Activate();
+		glUniform1f(uniID, 0.5f);
 
-		glUniform1f(uni, 0.5f);
 		// nối hình lại để chạy đc hình
-		kirby.Bind();
+		popCat.Bind();
 		vao.Bind();
-
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -78,8 +82,8 @@ int main()
 	vao.Delete();
 	vbo.Delete();
 	ebo.Delete();
+	popCat.Delete();
 	shader.Delete();
-	kirby.Delete();
 
 	glfwDestroyWindow(window);
 	glfwTerminate();

b50dcf7dc2f11d7bb3a365aa0be350abf390bcd6
Keith Lê <87120816+LeDongAnhKiet@users.noreply.github.com>
2023-05-17 17:41:12 +0700

    Chạy hình ảnh lên window

--- 06 - Hình ảnh/Texture.cpp
+++ 06 - Hình ảnh/Texture.cpp
@@ -1,15 +1,16 @@
-﻿#include "Texture.h"
+#include "Texture.h"
 
 Texture::Texture(const char* img, GLenum txType, GLenum slot, GLenum fm, GLenum pxType)
 {
 	// gán loại ảnh cho đối tượng texture
 	type = txType;
+
 	// lấy độ dài, độ rộng, chỉ số loại màu của ảnh
-	int widImg, hgtImg, nColCh;
+	int widImg, hegImg, nColCh;
 	// lật ảnh từ dưới lên
 	stbi_set_flip_vertically_on_load(true);
 	// đọc dữ liệu ảnh
-	unsigned char* byte = stbi_load(img, &widImg, &hgtImg, &nColCh, 0);
+	unsigned char* bytes = stbi_load(img, &widImg, &hegImg, &nColCh, 0);
 
 	// khởi tạo đối tượng Texture
 	glGenTextures(1, &ID);
@@ -18,32 +19,38 @@ Texture::Texture(const char* img, GLenum txType, GLenum slot, GLenum fm, GLenum 
 	glBindTexture(txType, ID);
 
 	// cấu hình lại loại giải thuật dùng cho hình ảnh
-	glTexParameteri(txType, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(txType, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(txType, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(txType, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
 	// cấu hình cách lặp của hình (nếu đc)
-	glTexParameteri(txType, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(txType, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(txType, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+	glTexParameteri(txType, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
 	/*		// Nếu dù GL_CLAMP_TO_BORDER
 	float fClr[] = { 1.0f, 1.0f, 1.0f, 1.0f; }
 	glTexParameterfv (txType, GL_TEXTURE_BORDER_COLOR, fClr);	*/
 
 	// gán ảnh vào đối tượng OpenGL Texture
-	glTexImage2D(txType, 0, GL_RGBA, widImg, hgtImg, 0, fm, pxType, byte);
+	glTexImage2D(txType, 0, GL_RGBA, widImg, hegImg, 0, fm, pxType, bytes);
 	// tạo MipMaps
 	glGenerateMipmap(txType);
+
 	// xóa ảnh vì OpenGL Texture đã lưu đc
-	stbi_image_free(byte);
+	stbi_image_free(bytes);
+
 	// ngắt nối để ko xảy ra lỗi ngoài ý muốn
 	glBindTexture(txType, 0);
 }
 
-void Texture::txUnit(Shader shade, const char* uni, GLuint unit)
+void Texture::texUnit(Shader& shader, const char* uni, GLuint unit)
 {
-	GLuint txUni = glGetUniformLocation(shade.ID, uni);
+	// Lấy vị trí của uniform
+	GLuint txUni = glGetUniformLocation(shader.ID, uni);
 	// kích hoạt Shader TRƯỚC khi đổi giá trị uniform
-	shade.Activate();
+	shader.Activate();
+	// Gán giá trị vào uniform
 	glUniform1i(txUni, unit);
 }
+
 void Texture::Bind() { glBindTexture(type, ID); }
 
 void Texture::Unbind() { glBindTexture(type, 0); }
